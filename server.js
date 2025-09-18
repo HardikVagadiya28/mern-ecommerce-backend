ff038cd81f0c1b48c79c08c8b1ee1e81063e020b
hardik-orwetry <hardik.orwetry@gmail.com>
2025-09-18 20:46:01 +0530

    Initial commit - backend code

--- server.js
+++ server.js
@@ -48,6 +48,5 @@ mongoose
   .then(() => console.log("Mongodb Connected Successfully...!"))
   .catch((err) => console.log(err));
 
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
